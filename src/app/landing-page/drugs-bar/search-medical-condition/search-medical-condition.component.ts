a10d392deb94a9a6d7527815853945ab7cb15f97
CharlotteSimons <charlottesimons99@gmail.com>
2024-01-19 13:11:58 +0100

    Medical condition to resultspage

--- src/app/landing-page/drugs-bar/search-medical-condition/search-medical-condition.component.ts
+++ src/app/landing-page/drugs-bar/search-medical-condition/search-medical-condition.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchInputService } from '../../../services/search-input.service'; // Pas dit pad aan indien nodig
 
 @Component({
   selector: 'app-search-medical-condition',
   templateUrl: './search-medical-condition.component.html',
-  styleUrls: [ // Dit moet 'styleUrls' zijn, niet 'styleUrl'
+  styleUrls: [
     './search-medical-condition.component.css',
     '../../../shared/styles/shared-styles.css' // Zorg dat dit pad correct is
   ]
@@ -13,7 +14,16 @@ export class SearchMedicalConditionComponent {
   showSuggestions: boolean = true;
   showSidePanelMedicalCondition: boolean = false;
 
+  constructor(private searchInputService: SearchInputService) {}
+
   handleInfoClick(): void {
     this.showSidePanelMedicalCondition = !this.showSidePanelMedicalCondition;
+  }
+
+  onSearchTermChange(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement && inputElement.value) {
+      this.searchInputService.changeSearchTerm(inputElement.value);
+    }
   }
 }
--- src/app/results-page/results-table/results-table.component.ts
+++ src/app/results-page/results-table/results-table.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SearchInputService } from '../../services/search-input.service'; // Pas dit pad aan indien nodig
 
 @Component({
   selector: 'app-results-table',
   templateUrl: './results-table.component.html',
   styleUrls: ['./results-table.component.css']
 })
-export class ResultsTableComponent {
+export class ResultsTableComponent implements OnInit {
+  searchTerm: string = '';
   items = [
     { veld1: 'Toripalimab injection', veld2: 'PD-1 receptor, EGFR-mutation.', veld3: 'Phase 3', veld4: '40 patients were enrolled with an overall ORR of 50.0% and disease-control rate (DCR) of 87.5%. The median progression free survival (PFS) and overall survival were 7.0 and 23.5 months, respectively.', veld5: 'The most common (≥20%) TRAE included 33 (82.5%) leukopenia, 28 (70.0%) neutropenia, 27 (67.5%) anemia, 21 (52.5%) elevated AST, 20 (50.0%) elevated ALT, 19 (47.5%) nausea, 19 (47.5%) thrombocytopenia, 15 (37.5%) decreased appetite, 11 (27.5%) constipation, and 10 (25.0%) asthenia.', veld6:'2 clinical studies recruiting' },
     { veld1: 'Pembrolizumab 25 MG/ML', veld2: 'PD-1 receptor, EGFR-mutation.', veld3: 'Phase 3', veld4: '40 patients were enrolled with an overall ORR of 50.0% and disease-control rate (DCR) of 87.5%. The median progression free survival (PFS) and overall survival were 7.0 and 23.5 months, respectively.', veld5: 'The most common (≥20%) TRAE included 33 (82.5%) leukopenia, 28 (70.0%) neutropenia, 27 (67.5%) anemia, 21 (52.5%) elevated AST, 20 (50.0%) elevated ALT, 19 (47.5%) nausea, 19 (47.5%) thrombocytopenia, 15 (37.5%) decreased appetite, 11 (27.5%) constipation, and 10 (25.0%) asthenia.', veld6:'3 clinical studies recruiting' },
@@ -14,7 +16,13 @@ export class ResultsTableComponent {
     { veld1: 'Nivolumab', veld2: 'PD-1 receptor, EGFR-mutation.', veld3: 'Phase 3', veld4: '40 patients were enrolled with an overall ORR of 50.0% and disease-control rate (DCR) of 87.5%. The median progression free survival (PFS) and overall survival were 7.0 and 23.5 months, respectively.', veld5: 'The most common (≥20%) TRAE included 33 (82.5%) leukopenia, 28 (70.0%) neutropenia, 27 (67.5%) anemia, 21 (52.5%) elevated AST, 20 (50.0%) elevated ALT, 19 (47.5%) nausea, 19 (47.5%) thrombocytopenia, 15 (37.5%) decreased appetite, 11 (27.5%) constipation, and 10 (25.0%) asthenia.', veld6:'1 clinical study recruiting' },
   ];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private searchInputService: SearchInputService) {}
+
+  ngOnInit() {
+    this.searchInputService.currentSearchTerm.subscribe(term => {
+      this.searchTerm = term;
+    });
+  }
 
   redirectToDrugPage(drugName: string) {
     this.router.navigate(['/drugpage', drugName]);
